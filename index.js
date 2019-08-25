e67a9cdfaf3386e3eea133dd2d2e86dc3b01607d
Truong Ngoc Huyen <trg.ngochuyen@gmail.com>
2019-08-25 12:48:31 +0300

    Submit exercise 3.4

--- index.js
+++ index.js
@@ -38,11 +38,17 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(person => person.id === id)
-    if (note) {
+    if (person) {
         res.json(person)
     } else {
         res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
 })
 
 app.get('/info', (req, res) => {
